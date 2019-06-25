0caa15bf068ba872bb8708d8d2909c49771ca8a8
xujiayi <kyzy1010@outlook.com>
2019-06-25 19:34:15 +0800

    hlllewoiiew

---
+++ src/main/java/com/atguigu/jianfa0105/NewJianfa.java
@@ -0,0 +1,11 @@
+package com.atguigu.jianfa0105;
+
+/**
+ * @author shkstart
+ * @create 2019-06-25 19:32
+ */
+public class NewJianfa {
+    public static void main(String[] args) {
+        System.out.println("hello new jianfa");
+    }
+}
