3ff510f0685af65119001843bd3b49e2b7a3cdc6
xujiayi <kyzy1010@outlook.com>
2019-06-25 19:07:36 +0800

    hello21

---
+++ src/main/java/com/atguigu/jianfa0105/Dugujiujian.java
@@ -0,0 +1,11 @@
+package com.atguigu.jianfa0105;
+
+/**
+ * @author shkstart
+ * @create 2019-06-25 16:59
+ */
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("hello dugujiujian");
+    }
+}
---
+++ src/main/java/com/atguigu/jianfa0105/Huashan.java
@@ -0,0 +1,11 @@
+package com.atguigu.jianfa0105;
+
+/**
+ * @author shkstart
+ * @create 2019-06-25 18:41
+ */
+public class Huashan {
+    public static void main(String[] args) {
+        System.out.println("huashan");
+    }
+}
